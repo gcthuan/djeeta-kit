c08e67a3c27de03db1bd4d040b61a5b5b212f335
gcthuan <gcthuan118@gmail.com>
2017-01-15 23:51:27 -0800

    Added button select to images by character name

--- js/myscript.js
+++ js/myscript.js
@@ -29,14 +29,14 @@ $(document).ready(function() {
 		clearInterval(myInterval);
 	}
 
-	chrome.runtime.onMessage.addListener(
-		function(request, sender, sendResponse) {
-		    console.log(sender.tab ?
-		                "from a content script:" + sender.tab.url :
-		                "from the extension");
-		    if (request.greeting == "hello")
-		      sendResponse({farewell: "goodbye"});
-	});
+	// chrome.runtime.onMessage.addListener(
+	// 	function(request, sender, sendResponse) {
+	// 	    console.log(sender.tab ?
+	// 	                "from a content script:" + sender.tab.url :
+	// 	                "from the extension");
+	// 	    if (request.greeting == "hello")
+	// 	      sendResponse({farewell: "goodbye"});
+	// });
 });
 
 
--- js/popup.js
+++ js/popup.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
 	const path = "data.json";
 	LoadCollection(path);
 
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    	console.log(response);
-  	});
-});
+	$("li").click(function(v) {
+		alert("hi");
+		console.log(v.target.innerText);
+	});
 
+	// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+ //  		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+ //    		console.log(response);
+ //  		});
+	// });
+
+	
 
 	// get images collection from data.json and show in popup
 	function LoadCollection(path) {
@@ -15,17 +21,33 @@ $(document).ready(function() {
 		    url : path,
 		    success: function (responseData) {
 		    	const parsedData = JSON.parse(responseData);
-		    	charList = parsedData.characters
+		    	const charList = parsedData.characters_list;
+		    	const characters = parsedData.characters;
 		    	for (let key in charList) {
-		    		const imagesArr = charList[key].images;
-		    		for (let key in imagesArr) {
-		    			console.log(imagesArr[key]);
-		    			const html = '<div class="col-xs-4"><img class="img-col" src='
-		    			+ imagesArr[key] + "></div>";
-		    			$(".row").append(html);
-		    		}
+		    		// const imagesArr = charList[key].images;
+		    		const dropdownHtml = '<li><a href="#">' + charList[key] + '</a></li>'
+		    		$(".dropdown-menu").append(dropdownHtml);
 		    	}
+		    	$("li").click(function(v) {
+		    		$("#button-value")[0].innerText = v.target.innerText + " ";
+		    		$("#collection").empty();
+		    		LoadCharacter(characters, v.target.innerText);
+		    	})
 		    }
 		});
+	}
+
+	function LoadCharacter(characters, name) {
+		const charImages = FindCharacter(characters, name)[0].images;
+		for (let key in charImages) {
+			const collectionHtml = '<div class="col-xs-4"><img class="img-col" src=' + charImages[key] + '></div>';
+			$("#collection").append(collectionHtml);
+		}
+	}
+
+	function FindCharacter(characters, name) {
+		return characters.filter(function(data) {
+			return data.name == name;
+		})
 	}
 });
